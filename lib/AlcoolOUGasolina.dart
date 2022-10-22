cc5202395c141d23a065b8b241df1d638e22a938
p8 <phpdias@outlook.com>
2022-10-22 16:03:01 -0300

    Update AlcoolOUGasolina.dart

--- lib/AlcoolOUGasolina.dart
+++ lib/AlcoolOUGasolina.dart
@@ -8,6 +8,10 @@ class AlcoolOUGasolina extends StatefulWidget {
 }
 
 class _AlcoolOUGasolinaState extends State<AlcoolOUGasolina> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,67 +21,99 @@ class _AlcoolOUGasolinaState extends State<AlcoolOUGasolina> {
       ),
 
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(35),
-              child: Column(
-                children: <Widget>[
-
-                  Image.asset(
-                    "images/logo.png",
-                    alignment: Alignment.center,
-                    width: 350,
-                  ),
-
-                  Text(
-                    "Saiba qual a melhor opção para abastecimento do seu carro",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
 
 
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: "Preço do Álcool (ex: 1.59):",
-                    ),
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 25,
-                      color: Colors.blue,
-                    ),
-                  ),
-
-
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: "Preço da Gasolina (ex: 3.15):",
-                    ),
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 25,
-                      color: Colors.blue,
-                    ),
-                  ),
-
-
-                  ElevatedButton(
-                    onPressed: (){},
-                    child: Text("Calcular"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.lightBlue,
-                    ),
-                  ),
-
-                ],
+              //imagem
+              Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 32),
+                child: Image.asset(
+                  "images/logo.png",
+                  alignment: Alignment.center,
+                  width: 350,
+                ),
               ),
-            ),
 
 
-          ],
+              //texto
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+
+
+              //alcool
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço do Álcool (ex: 1.59):",
+                ),
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 25,
+                  color: Colors.blue,
+                ),
+                controller: _controllerAlcool,
+              ),
+
+
+              //gasolina
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço da Gasolina (ex: 3.15):",
+                ),
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 25,
+                  color: Colors.blue,
+                ),
+                controller: _controllerGasolina,
+              ),
+
+
+              //botão
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue,
+                  ),
+                ),
+              ),
+
+
+              //resultado
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+
+
+            ],
+          ),
         ),
       ),
     );
